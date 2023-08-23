9a847425bd4660f83220b11dd8bf1f47f29e9090
Sunnat <sunnatbekamirov22@gmail.com>
2023-08-24 00:57:54 +0500

    Simple Test Cases Added.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y, and returns the result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y from x returns result."""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,22 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        result = calc.add(5, 8)
+
+        self.assertEqual(result, 13)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        result = calc.subtract(15, 5)
+
+        self.assertEqual(result, 10)
